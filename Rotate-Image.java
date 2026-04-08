06def72b765827851d4af67a2f2b1fd4960fd6c9
Abhinav Singh <abhinavcs201@gmail.com>
2026-04-08 17:11:50 +0530

    [48] [Time Beats: 100.00%] [Memory Beats: 41.40%] - LeetPush

--- Rotate-Image.java
+++ Rotate-Image.java
@@ -1,24 +1,24 @@
 1class Solution {
 2    public void rotate(int[][] matrix) {
-3        int n = matrix.length;
-4        int A[][] = new int[n][n];
-5
-6     int r = 0;
-7     int c = 0;
-8     for(int j = 0;j<n;j++){
-9        for(int i = n-1;i>=0;i--){
-10A[r][c] = matrix[i][j];
-11c++;
-12if(c==n){
-13    c = 0;
-14    r++;
-15}
-16        }
-17     }
-18      for (int i = 0; i < n; i++) {
-19            for (int j = 0; j < n; j++) {
-20                matrix[i][j] = A[i][j];
-21            }
-22        }
+3     int n = matrix.length;
+4     for(int i = 0; i<n-1;i++){
+5        for(int j = i+1;j<n;j++){
+6            int temp = matrix[i][j];
+7            matrix[i][j] = matrix[j][i];
+8            matrix[j][i] = temp;
+9
+10        }
+11
+12     }
+13
+14     for(int i = 0;i<n;i++){
+15        for(int j = 0;j<n/2;j++){
+16int temp =            matrix[i][j];
+17matrix[i][j] = matrix[i][n-j-1];
+18matrix[i][n-j-1] = temp;
+19        
+20        
+21        }
+22     }
 23    }
 24}
